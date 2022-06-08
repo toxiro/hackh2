f71eda68736fb5d235a1e19d545fbf6a1073a3b8
Dilmo Hideki Shibata <A0043550@redecorp.br>
2022-06-08 09:42:17 -0300

    Alterado o testo de retorno

--- GetProductNameHacker2/index.js
+++ GetProductNameHacker2/index.js
@@ -3,8 +3,8 @@ module.exports = async function (context, req) {
 
     const productId = (req.query.productId);
     const responseMessage = productId
-    ? "This starfruit ice cream is out of this world!"
-  //      ? "The product name for your product id " + productId + " is Starfruit Explosion"
+   // ? "This starfruit ice cream is out of this world!"
+        ? "The product name for your product id " + productId + " is Starfruit Explosion"
         : "This HTTP triggered function executed successfully. Pass a productId in the query string for a personalized response.";
 
     context.res = {
